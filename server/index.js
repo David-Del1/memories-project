56b89773d5fcbcdd4891e1c556db4dc828b60082
David Delgadillo <daviddelgadillo@davids-mbp.lan>
2021-09-29 10:22:35 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import PostRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', PostRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// MongoDB Atlas
+const CONNECTION_URL = 'mongodb+srv://david:david123@cluster0.sye1n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+  .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+  .catch(err => console.error(err.message));
