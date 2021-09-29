34bb28596a6bbee8803ef9d1dfd2302cf1c00fa0
David Delgadillo <daviddelgadillo@davids-mbp.lan>
2021-09-29 14:34:59 -0700

    Successfully installed and configured redux

---
+++ client/src/components/Forms/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import useStyles from './styles.js';
+
+function Form() {
+  const classes = useStyles();
+  return (
+    <div>
+      FORM
+    </div>
+  )
+}
+
+export default Form;
---
+++ client/src/components/Posts/Post/Post.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import useStyles from './styles.js';
+
+function Post() {
+  const classes = useStyles();
+  return (
+    <div>
+      POST
+    </div>
+  )
+}
+
+export default Post
